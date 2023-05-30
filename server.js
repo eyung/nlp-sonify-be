125a5c21a2e668574f6a952c7b85103e78a274fa
eyung <effiamyung@gmail.com>
2023-05-30 09:28:43 -0700

    added line app.use(express.json());

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const natural = require('natural');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 // Create an instance of Natural NLP components (e.g., tokenizer, stemmer, etc.)
 const tokenizer = new natural.TreebankWordTokenizer();
@@ -11,7 +12,6 @@ const testString = "this is a test";
 
 // Define API endpoints
 app.get('/api/test', (req, res) => {
-  // Handle the request and send a response
   res.json({ message: 'Hello, world!' });
 });
 
@@ -23,7 +23,8 @@ app.post('/api/tokenize', (req, res) => {
 });
 
 app.post('/api/stem', (req, res) => {
-  const word = req.query.word;
+  const word = req.body.word;
+  console.log("Word : " + word);
   const stemmedWord = stemmer.stem(word);
   res.json(stemmedWord);
 });

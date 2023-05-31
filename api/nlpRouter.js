d79f21c537bc022998f39472da668a0c1e5d1569
eyung <effiamyung@gmail.com>
2023-05-31 11:47:21 -0700

    created /api/pos/tags endpoing in nlpRouter

--- api/nlpRouter.js
+++ api/nlpRouter.js
@@ -1,4 +1,5 @@
 const natural = require('natural');
+const pos = require('pos');
 const express = require('express');
 
 // Create an instance of the Express router
@@ -26,6 +27,31 @@ router.get('/api/stem', (req, res) => {
   const stemmedWord = stemmer.stem(word);
   res.json(stemmedWord);
 });
+
+router.get('/api/pos/tags', (req, res) => {
+    const sentence = req.body.sentence;
+
+    if (!sentence) {
+        res.status(400).json({ error: 'Missing sentence in request body' });
+        return;
+    }
+
+    console.log("Sentence : " + sentence);
+
+    // Tokenize the sentence into words
+    const words = tokenizer.tokenize(sentence);
+
+    const taggedWords = words.map((word) => {
+        const tagger = new pos.Tagger();
+        const taggedWord = tagger.tag([word])[0];
+        const wordText = taggedWord[0];
+        const tag = taggedWord[1];
+    
+        return { word: wordText, tag };
+      });
+    
+      res.json(taggedWords);
+  });
 
 // Export the router
 module.exports = router;

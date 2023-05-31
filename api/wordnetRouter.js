fb3cb6a7a8b507823642dbc18953ca0b55e2e1cc
eyung <effiamyung@gmail.com>
2023-05-31 10:08:54 -0700

    created wordnet api to get lexname of a word

--- api/wordnetRouter.js
+++ api/wordnetRouter.js
@@ -7,42 +7,23 @@ const router = express.Router();
 var wordnet = new natural.WordNet();
 
 // Define an API endpoint handler function
-router.get('/api/wordnet', (req, res) => {
-    
-    const text = req.body.text;
-    console.log("Text : " + text);
-
-    wordnet.lookup(text, function(results) {
-        results.forEach(function(result) {
-            console.log('------------------------------------');
-            //console.log(result.synsetOffset);
-            //console.log(result.pos);
-            //console.log(result.lemma);
-            //console.log(result.synonyms);
-            //console.log(result.pos);
-            //console.log(result.gloss);
-
-            const synsets = "hi"
-        });
-    });
-  
-    // Return the result as a JSON response
-    res.json(synsets);
-  });
-
-  router.get('/api/wordnet-test/:word', async (req, res) => {
+  router.get('/api/wordnet/lexnames/:word', async (req, res) => {
     const word = req.params.word;
-  
-    // Use the WordNet module to perform desired operations
-    // For example, find synsets (synonyms) for a given word
-    wordnet.lookup(word, (err, synsets) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'An error occurred' });
+
+    // Use the WordNet module to find all lexnames of the word
+    wordnet.lookup(word, (errs, results) => {
+      if (errs) {
+        console.error(errs);
+        //res.json({ error: err });
+        const lexnames = errs?.map((err) => err.lexFilenum);
+        res.json({ word, lexnames });
+      } else if (results.length === 0) {
+        res.status(404).json({ error: 'Word not found in WordNet' });
       } else {
-        // Extract the synonyms from the synsets
-        const synonyms = synsets.map((synset) => synset.words).flat();
-        res.json({ word });
+        // Extract the lexnames from the results
+
+        const lexnames = results?.map((result) => result.lexname);
+        res.json({ word, lexnames });
       }
     });
   });

b1d87920b805ca9e002d7764704210125897aef5
eyung <effiamyung@gmail.com>
2024-08-21 14:53:15 -0700

    test

--- api/aiRouter.js
+++ api/aiRouter.js
@@ -82,7 +82,7 @@ router.post('/v2/scores', async (req, res) => {
     "max_tokens": 1050,
     "presence_penalty": 0,
     "frequency_penalty": 0,
-    "response_format": zodResponseFormat(scores, "sentences"),
+    //"response_format": zodResponseFormat(scores, "sentences"),
     //"response_format":{"type": "json_object"}
   };
 

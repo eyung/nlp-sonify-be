8b7c6ee0c7ebd14d8b3e7645f55e685ea29403ba
eyung <effiamyung@gmail.com>
2024-08-28 11:11:08 -0700

    v3/scores to use structured outputs

--- api/aiRouter.js
+++ api/aiRouter.js
@@ -93,4 +93,45 @@ router.post('/v2/scores', async (req, res) => {
   }
 });
 
+
+// Endpoint for scores related to text analysis
+// Using OpenAI Structured Outputs as response format
+// Using GPT 4o mini model
+router.post('/v3/scores', async (req, res) => {
+  const { text } = req.body;
+  const systemMessage = "For each sentence or phrase, create a collection of JSON objects where the keys are the first word of each sentence, the values are the following: 1) Complexity Score from -1.0 to 1.0 where 1.0 indicates the highest level of complexity and -1.0 indicates the lowest; 2) Sentiment Analysis Score from -1.0 to 1.0, consider a wide range of factors, including but not limited to lexical, contextual and structural sentiments; 3) Concreteness Score from -1.0 to 1.0 where 1.0 indicates that the text refers to tangible things and -1.0 indicates abstractiveness; 4) Emotional-Intesity Score from -1.0 to 1.0 where 1.0 indicates a high level of emotional intensity and -1.0 indicates lowest level.";
+  
+  const prompt = {
+    "model": "gpt-4o-mini",
+    "messages": [
+      {
+        "role": "system",
+        "content": systemMessage
+      },
+      {
+        "role": "user",
+        "content": text
+      }
+    ],
+    "temperature": 0,
+    "top_p": 1,
+    "n": 1,
+    "stream": false,
+    "max_tokens": 950,
+    "presence_penalty": 0,
+    "frequency_penalty": 0,
+    "response_format": zodResponseFormat(scores, "scores"),
+    //"response_format": { "type": "json_object" },
+  };
+
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', prompt, { headers: { 'Authorization': `Bearer ${process.env.OPENAI_API_KEY}` } });
+    const validatedData = scores.parse(response.data);
+    res.json(validatedData);
+    //res.json(response.data);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
+
 export default router;
